aa0c6c25b35fda9342253d5cd04e30fed1e39869
Rohan Paramanand <rohanparamanand@gmail.com>
2024-11-30 19:22:22 +0530

    Began work

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def home():
-    return "Check"
+    return render_template('index.html')
+
+@app.route('/City')
+def choose_city():
+    pass
 
 if __name__ == "__main__":
     app.run(debug=True)
